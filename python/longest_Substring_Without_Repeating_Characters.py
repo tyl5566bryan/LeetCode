ee6fd0c1434597c5fa271e518814d108a40dfd6a
tyl5566bryan <tyl5566bryan@users.noreply.github.com>
2016-09-17 13:57:02 +0800

    modify variable name

--- python/longest_Substring_Without_Repeating_Characters.py
+++ python/longest_Substring_Without_Repeating_Characters.py
@@ -4,18 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dict = {}
+        myDict = {}
         start_id = 0
         max_len = 0
         
         for i in range(0, len(s)):
-            if s[i] in dict:
-                if dict[s[i]] >= start_id:
+            if s[i] in myDict:
+                if myDict[s[i]] >= start_id:
                     this_len = i - start_id
                     if this_len > max_len:
                         max_len = this_len
-                    start_id = dict[s[i]] + 1
-            dict[s[i]] = i
+                    start_id = myDict[s[i]] + 1
+            myDict[s[i]] = i
             
         final_len = len(s) - start_id
         if final_len > max_len:
